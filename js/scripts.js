8872b28f15aba2229097e7aecd3cda9cab0435fa
juancholes <juansalustio@hotmail.com>
2022-05-28 12:35:34 -0300

    cambio index

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+
+    const cantidad = document.getElementById('cantidad')
+
+    const resumen = document.getElementById('resumen')
+
+    const categoria = document.getElementById('inputstate')
+
+    const total = document.getElementById('total')
+
+
+    function comprar ()
+{
+    console.log(categoria.value)
+
+    console.log(cantidad.value)
+
+    cantidad.value
+
+    total.innerHTML= cantidad.value
+
+}
